47fe786e34c3ed55be83f4173b5b2d02899498eb
Victor Kamei Kay <kameikay@gmail.com>
2022-11-21 20:20:59 -0300

    add product handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/kameikay/api_example/configs"
+	"github.com/kameikay/api_example/infra/database"
+	"github.com/kameikay/api_example/infra/webserver/handlers"
+	"github.com/kameikay/api_example/internal/entities"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	configs, err := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(configs)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entities.Product{}, &entities.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
---
+++ infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/kameikay/api_example/infra/database"
+	"github.com/kameikay/api_example/internal/dto"
+	"github.com/kameikay/api_example/internal/entities"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entities.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
